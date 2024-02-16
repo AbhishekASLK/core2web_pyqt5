7006af55e469641d868c8f5f59657058c6768bfb
AbhishekASLK <bhoslea925@gmail.com>
2024-02-17 01:09:21 +0530

    user-profile initialized

---
+++ user_profile_form/main.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from Home.home import C2W_UserProfileForm
+from PyQt5 import QtGui
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = C2W_UserProfileForm()
+    ex.setWindowIcon(QtGui.QIcon('./assets/logo.png'))
+    ex.setWindowTitle('User Info')
+    ex.setGeometry(100, 100, 1300, 800)
+    ex.show()
+    sys.exit(app.exec_())
+
